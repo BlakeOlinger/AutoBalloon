fd239bb5215d961ad437b12857d7368188956de7
Blake Olinger <blake.olinger@symbosoft.com>
2019-10-25 15:48:28 -0400

    -- added ability to suppress cover to get all hidden features ballooned

--- AutoBalloon/Program.cs
+++ AutoBalloon/Program.cs
@@ -1,5 +1,6 @@
 ﻿using SldWorks;
 using SwConst;
+using System.Threading;
 
 namespace AutoBalloon
 {
@@ -8,10 +9,48 @@ namespace AutoBalloon
         static void Main(string[] args)
         {
             var swInstance = new SldWorks.SldWorks();
+            int errors = 0;
 
-            var model = (ModelDoc2)swInstance.ActiveDoc;
+            // must suppress cover to balloon hidden features
+            var delay = 300;
+            var assemblyName = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\blob - L2\blob.L2_cover.SLDASM";
+            var model = (ModelDoc2)swInstance.ActivateDoc3(assemblyName, true,
+                (int)swRebuildOnActivation_e.swRebuildActiveDoc, ref errors);
+            Thread.Sleep(delay);
+            // write to blob.L2_cover.txt '0' to the field "Is Dimensioned"
+            var coverConfigPath = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\blob - L2\blob.L2_cover.txt";
+            var coverConfigLines = System.IO.File.ReadAllLines(coverConfigPath);
+            var index = 0;
+            var stop = false;
+            while (!stop)
+            {
+                var line = coverConfigLines[index];
+
+                if (line.Contains("Is Dimensioned 2 Bool") &&
+                    !line.Contains("IIF"))
+                {
+                    stop = true;
+                    var newLine = coverConfigLines[index].Replace("1", "0");
+
+                    coverConfigLines[index] = newLine;
+                }
+                else
+                {
+                    ++index;
+                }
+            }
+            System.IO.File.WriteAllLines(coverConfigPath, coverConfigLines);
+            Thread.Sleep(delay);
+            model.ForceRebuild3(false);
+            Thread.Sleep(delay);
+
+            // switch to drawing
+            var coverDrawingFileName = @"C:\Users\bolinger\Documents\SolidWorks Projects\Prefab Blob - Cover Blob\base blob - L1\blob.cover.SLDDRW";
+            model = (ModelDoc2)swInstance.ActivateDoc3(coverDrawingFileName, true,
+                (int)swRebuildOnActivation_e.swRebuildActiveDoc, ref errors);
+
+            // draw balloons
             var drawing = (DrawingDoc)model;
-
             var boolStatus = drawing.ActivateView("Drawing View1");
 
             boolStatus = model.Extension.SelectByID2("Drawing View1",
@@ -46,6 +85,25 @@ namespace AutoBalloon
             }
 
             model.ClearSelection2(true);
+
+            Thread.Sleep(delay);
+
+            // switch back to assembly and unsuppress cover
+            model = (ModelDoc2)swInstance.ActivateDoc3(assemblyName, true,
+                (int)swRebuildOnActivation_e.swRebuildActiveDoc, ref errors);
+            Thread.Sleep(delay);
+            // write to config
+            coverConfigLines[index] = coverConfigLines[index].Replace("0", "1");
+            System.IO.File.WriteAllLines(coverConfigPath, coverConfigLines);
+            Thread.Sleep(delay);
+            model.ForceRebuild3(false);
+            Thread.Sleep(delay);
+            model.ForceRebuild3(false);
+            Thread.Sleep(delay);
+
+            // back to drawing
+            swInstance.ActivateDoc3(coverDrawingFileName, true,
+                (int)swRebuildOnActivation_e.swRebuildActiveDoc, ref errors);
         }
     }
 }
